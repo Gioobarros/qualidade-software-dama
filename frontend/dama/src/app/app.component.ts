f3fbc21308ff07dbfd7c45d30020491d619a9d29
Leandro <lesizilio2gmail.com>
2024-11-24 20:57:21 -0300

    Correção de erros

--- frontend/dama/src/app/app.component.ts
+++ frontend/dama/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CadastroComponent } from './cadastro/cadastro.component'; 
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HeaderComponent } from "./components/header/header.component"; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ CadastroComponent, RouterOutlet],
+  imports: [CadastroComponent, RouterOutlet, HeaderComponent],
   template: `
+  <app-header></app-header>
   <app-cadastro></app-cadastro>
   <router-outlet></router-outlet>
   
