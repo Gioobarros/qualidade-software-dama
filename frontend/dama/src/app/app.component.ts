b181a7a408fb223f8c1bac76da77f92c5b560f47
Leandro <lesizilio2gmail.com>
2024-11-24 16:54:41 -0300

    Organização de Login

--- frontend/dama/src/app/app.component.ts
+++ frontend/dama/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ CadastroComponent], 
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  imports: [ CadastroComponent, RouterOutlet],
+  template: `
+  <app-cadastro></app-cadastro>
+  <router-outlet></router-outlet>
+  
+  `
 })
 export class AppComponent {
   title = 'dama';
