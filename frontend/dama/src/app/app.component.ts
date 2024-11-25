1fa4fe13e632f0b07f62620d3d8f303864ec1069
Leandro <lesizilio2gmail.com>
2024-11-25 02:21:01 -0300

    Listar, Deletar, Logar, sem ligação com o Back. Cadastro com Ligação com o Backend

--- frontend/dama/src/app/app.component.ts
+++ frontend/dama/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { HeaderComponent } from "./components/header/header.component"; 
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CadastroComponent, RouterOutlet, HeaderComponent],
+  imports: [CadastroComponent, RouterOutlet, HeaderComponent, ListComponent],
   template: `
   <app-header></app-header>
+  <app-list></app-list>
   <app-cadastro></app-cadastro>
   <router-outlet></router-outlet>
   
