c1813db6572411dceafa06738f71767b48fc5dae
Jacques Gomes <jacquesgomes66@gmail.com>
2024-11-26 09:26:02 -0300

    :sparkles: Adicionando html mais completo para cadastro

--- frontend/dama/src/app/cadastro/cadastro.component.ts
+++ frontend/dama/src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+  tipoCadastro: string = '';
+  cadastro: any = {
+    nome: '',
+    email: '',
+    senha: '',
+    contato: '',
+    cnpj: '',
+    bio: '',
+    cpf: '',
+    conselho: '',
+  };
 
+  onSubmit(form: any): void {
+    console.log('Formulário submetido:', form.value);
+  }
+
+  cancelar(): void {
+    console.log('Operação cancelada');
+  }
 }
