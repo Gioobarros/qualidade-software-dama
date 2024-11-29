e29fe0ab5de24f4961c45bc69a739c7d9d5be216
Leandro <lesizilio2gmail.com>
2024-11-29 11:14:19 -0300

    Remoção do PyCache

--- frontend/dama/src/app/app.routes.ts
+++ frontend/dama/src/app/app.routes.ts
@@ -11,6 +11,11 @@ export const routes: Routes = [
     {
       path: 'login',
       loadComponent: () => import('./login/login.component').then(c => c.LoginComponent)
+    },
+    {
+      path: 'cadastro',
+      loadComponent: () => import('./cadastro/cadastro.component').then(c => c.CadastroComponent)
     }
+
   ];
   
