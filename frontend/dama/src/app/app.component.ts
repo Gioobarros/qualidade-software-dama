891d4c1f14d53f0836ccc6745140304d935420ca
Leandro <lesizilio2gmail.com>
2024-12-04 01:02:36 -0300

    Adição da Marca Grafica e Icon

--- frontend/dama/src/app/app.component.ts
+++ frontend/dama/src/app/app.component.ts
@@ -3,14 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { HeaderComponent } from "./components/header/header.component"; 
 import { ListComponent } from './list/list.component';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CadastroComponent, RouterOutlet, HeaderComponent, ListComponent],
+  imports: [CadastroComponent, RouterOutlet, HeaderComponent, ListComponent, FooterComponent],
   template: `
   <app-header></app-header>
   <router-outlet></router-outlet>
+  <app-footer></app-footer>
   
   `
 })
