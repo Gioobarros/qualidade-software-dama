c8fa753f97e8bb3b7563951b7ad262a19666a9e1
Igor Santos <santos.igor1@escolar.ifrn.edu.br>
2024-12-09 15:51:56 -0300

    :rocket: Funcional!

--- backend/dama/api/serializer/profissional.py
+++ backend/dama/api/serializer/profissional.py
@@ -4,5 +4,5 @@ from api.models.profissional import Profissional
 class ProfissionalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profissional
-        fields = ['nome_completo', 'cpf', 'login', 'senha', 'conselho', 'contato', 'email', 'bio']
+        fields = ['id', 'nome_completo', 'cpf', 'login', 'senha', 'conselho', 'contato', 'email', 'bio']
         
