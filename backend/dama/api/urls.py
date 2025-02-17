4a5a1892e9cca2baf3f3056c2c515fec100245d4
Felipe-CF <prog.study.fcf@gmail.com>
2025-02-16 22:53:43 -0300

    fix: adaptação das rotas par teste dos filtros

--- backend/dama/api/urls.py
+++ backend/dama/api/urls.py
@@ -3,20 +3,17 @@ from api.view.administrador import AdministradorView
 from api.view.profissional import ProfissionalView
 from api.view.relato import RelatoView
 from api.view.ong import OngView
+from api.view.usuario import UsuarioView
 from api.view.auth import LoginView, LogoutView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'relato', RelatoView, basename='relato')
+router.register(r'ong', OngView, basename='ong')
+router.register(r'profissional', ProfissionalView, basename='profissional')
+router.register(r'admin', AdministradorView, basename='admin')
+router.register(r'usuario', UsuarioView, basename='usuario')
+router.register(r'login', LoginView, basename='login')
+router.register(r'logout', LogoutView, basename='logout')
 
-# http://127.0.0.1:8000/api/relato/?data_inicio=(yyyy-mm-dd)&data_fim=(yyyy-mm-dd)&palavra_chave=()
-
-urlpatterns = [
-    path('ong/', OngView.as_view(), name='ong'),
-    path('profissional/', ProfissionalView.as_view(), name='profissional'),
-    path('administrador/', AdministradorView.as_view(), name='administrador'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-]
-
-urlpatterns += router.urls
+# http://127.0.0.1:8000/api/relato/?data_inicio=(yyyy-mm-dd)&data_fim=(yyyy-mm-dd)&palavra_chave=&status
