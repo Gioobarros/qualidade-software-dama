f1c02873436ecc3c383f6987f5be9dc813436770
Felipe-CF <prog.study.fcf@gmail.com>
2025-02-18 07:49:20 -0300

    fix: correção no endpoint de alterar relato

--- backend/dama/api/view/relato.py
+++ backend/dama/api/view/relato.py
@@ -63,21 +63,27 @@ class RelatoView(viewsets.ModelViewSet):
 
     def partial_update(self, request, *args, **kwargs):
         try:
-            if 'id' in request.data and 'novo_conteudo' in request.data:
-                item = get_object_or_404(Relato, id=request.data.get("id"))
+            item = self.get_object()
 
-                serializer = self.get_serializer(item, data=request.data, partial=True)
+            serializer = self.get_serializer(
+                item, 
+                data=request.data, 
+                partial=True, 
+                context={
+                    'usuario': request.user
+                }
+                )
 
-                if serializer.is_valid():
-                    serializer.save()
+            if serializer.is_valid():
+                serializer.save()
 
-                    return Response(serializer.data, status=status.HTTP_200_OK)
-            else:
-                return Response({"erro": "campos obrigatórios ausentes na requisição"}, status=status.HTTP_406_NOT_ACCEPTABLE)
-            
+                return Response(serializer.data, status=status.HTTP_200_OK)
+
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
         except Exception as e:
             return Response({"erro": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    
+        
 
     def destroy(self, request, *args, **kwargs):
         try:
